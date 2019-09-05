dff3f117adeec49982d92b5ea4bea1d8d685654b
Diego Santana <diego_4palco@hotmail.com>
2019-09-05 17:11:11 -0300

    Criacao da classe Main, a nossa janela principal

---
+++ src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static int LARGURA = 1366, ALTURA = 768;
+	
+	public static void main (String [] args) {
+		
+		JFrame janela = new JFrame("SpaceInvaders");
+		janela.setSize(LARGURA, ALTURA);
+		janela.setVisible(true);
+		janela.setResizable(false);
+		//janela.setLocationRelativeTo(null); Descomente caso queira nascer a tela no centro
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
