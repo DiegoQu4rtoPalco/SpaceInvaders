6a1582828e9455ea0c4f2fd155e4ebf295294350
Diego Santana <diego_4palco@hotmail.com>
2019-09-06 16:27:11 -0300

    Modificando objeto Tiro para um ArrayList

--- src/application/SpaceInvaders.java
+++ src/application/SpaceInvaders.java
@@ -3,6 +3,7 @@ package application;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -11,13 +12,14 @@ public class SpaceInvaders extends JPanel implements Runnable, KeyListener {
 	private static final long serialVersionUID = 1L;
 	
 	private Nave nave;
-	private Tiro tiro;
+	private ArrayList <Tiro> tiro;
 
 	
 	public SpaceInvaders() {
 		Thread processoDoJogo = new Thread(this);
 		processoDoJogo.start();
 		nave = new Nave();
+		tiro = new ArrayList<Tiro>();
 		
 		
 	}
@@ -45,14 +47,15 @@ public class SpaceInvaders extends JPanel implements Runnable, KeyListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		nave.pintar(g);
-		if(tiro != null) {
-			tiro.pintar(g);
+		for(int i = 0; i < tiro.size(); i++) {
+			tiro.get(i).pintar(g);
 		}
+		
 	}
 
 	private void atualizar() {
-		if(tiro != null) {
-			tiro.direcaoY();
+		for(int i = 0; i < tiro.size(); i++) {
+			tiro.get(i).direcaoY();
 		}
 		
 	}
@@ -60,7 +63,7 @@ public class SpaceInvaders extends JPanel implements Runnable, KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
-			tiro = nave.atirar();
+			tiro.add(nave.atirar());
 		}
 		
 	}
